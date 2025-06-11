51e21a0b5c4a277960961967ca06d34b7ed00881
Morgan Favour <morganfavour85@gmail.com>
2025-06-11 22:34:33 +0100

    Refactor mobile menu functionality to improve toggle behavior and close on link click

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-function toggleMobileMenu() {
-  const menu = document.getElementById('mobile-menu');
-  menu.classList.toggle('open');
-}
+let isMenuOpen = false;
+
+  function toggleMobileMenu() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('open');
+    isMenuOpen = menu.classList.contains('open');
+  }
+
+  // Closes the menu on scroll
+  window.addEventListener('scroll', () => {
+    const menu = document.getElementById('mobile-menu');
+    if (isMenuOpen) {
+      menu.classList.remove('open');
+      isMenuOpen = false;
+    }
+  });
+
+//  functon closes the menu when a link is clicked 
+  document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('#mobile-menu a');
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        const menu = document.getElementById('mobile-menu');
+        menu.classList.remove('open');
+        isMenuOpen = false;
+      });
+    });
+  });
 
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1.2,
